ca2b1f31cd4789cb59b80e5f7b2ab48736236df9
FLL42 <team.FLL.42@gmail.com>
2019-10-23 18:21:48 -0400

    Swing comes back to base

--- Missions/Swing+Elevator.py
+++ Missions/Swing+Elevator.py
@@ -10,14 +10,15 @@ mt.on_for_rotations(SpeedPercent(20), SpeedPercent(20), .8) # ROBOT MOVES INTO S
 mt.on_for_rotations(SpeedPercent(-30), SpeedPercent(-30), 0.4) #ROBOT MOVES AWAY FROM SWING
 mt.on_for_rotations(SpeedPercent(-30), SpeedPercent(0), 1.5) #ROBOT TURNS TO SQUARE ON WALL
 motorA.on_for_degrees(SpeedPercent(15), 150) #LEFT ARM TURNS FOR ELEVATOR
-mt.on_for_rotations(SpeedPercent(-15), SpeedPercent(-15), 0.3) # ROBOT MOVES BACK INTO WALL
-mt.on_for_rotations(SpeedPercent(30), SpeedPercent(30), 1.8) 
-mt.on_for_rotations(SpeedPercent(30), SpeedPercent(0), 1) #
-mt.on_for_rotations(SpeedPercent(30), SpeedPercent(30), 1.25) 
-motorA.on_for_degrees(SpeedPercent(15), 50)
-motorA.on_for_degrees(SpeedPercent(15), 150)
-mt.on_for_rotations(SpeedPercent(0), SpeedPercent(-30), 0.8)
-mt.on_for_rotations(SpeedPercent(15), SpeedPercent(15), 1.05)
-mt.on_for_rotations(SpeedPercent(-10), SpeedPercent(10), 0.25)
-mt.on_for_rotations(SpeedPercent(-30), SpeedPercent(0), 0.5) 
-mt.on_for_rotations(SpeedPercent(-30), SpeedPercent(-30), 10) 
+mt.on_for_rotations(SpeedPercent(-15), SpeedPercent(-15), 0.45) # ROBOT MOVES BACK INTO WALL
+mt.on_for_rotations(SpeedPercent(30), SpeedPercent(30), 1.8) #ROBOT MOVES FORWARD TO ELEVATOR
+mt.on_for_rotations(SpeedPercent(30), SpeedPercent(0), 1) #ROBOT TURNS CLOCKWISE TO FACE ELEVATOR
+mt.on_for_rotations(SpeedPercent(30), SpeedPercent(30), 1.25) #ROBOT MOVES FORWARD AND HITS ELEVATOR
+motorA.on_for_degrees(SpeedPercent(15), 200)#MEDIUM MOTOR TURNS AWAY SO IT DOESENT UNDO ELEVATOR
+mt.on_for_rotations(SpeedPercent(0), SpeedPercent(-30), 0.8)#ROBOT TURNS TO SAFETY FACTOR
+mt.on_for_rotations(SpeedPercent(15), SpeedPercent(15), 1.13)#ROBOT MOVES INTO SAFETY FACTOR
+mt.on_for_rotations(SpeedPercent(10), SpeedPercent(-10), 0.2)#ROBOT TURNS TO KNOCK DOWN BEAMS
+mt.on_for_rotations(SpeedPercent(-15), SpeedPercent(-15), 0.1) # ROBOT MOVES BACK INTO WALL
+mt.on_for_rotations(SpeedPercent(-10), SpeedPercent(10), 0.5)#ROBOT TURNS TO KNOCK DOWN BEAMS
+mt.on_for_rotations(SpeedPercent(-30), SpeedPercent(0), 0.1) #ROBOT TURNS BACK TO BASE
+mt.on_for_rotations(SpeedPercent(-60), SpeedPercent(-60), 12) # ROBOT MOVES BACK TO BASE

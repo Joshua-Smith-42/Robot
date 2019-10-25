73ff798e4daf66917d853df0579e8e0384a26bb9
FLL42 <team.FLL.42@gmail.com>
2019-10-25 16:28:48 -0400

    Updated master program to latest code

--- Missions/MasterProgram.py
+++ Missions/MasterProgram.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+# FLL 42, Pythonian Rabbotics's master program. Copyright (c) 2019 FLL team 42
+
 #---------------------------------------------------Imports and variable definitions-----------------------------------------------------------------
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, SpeedPercent, MoveTank, MediumMotor
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.sensor.lego import GyroSensor
@@ -9,14 +11,15 @@ from ev3dev2.button import Button
 from ev3dev2.sound import Sound
 from ev3dev2.display import Display
 import time
+import sys
 btn = Button()
 color = ColorSensor(INPUT_4)
 tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)  #This is the template whenever we code
+motorA = MediumMotor(OUTPUT_A)
+motorD = MediumMotor(OUTPUT_D)
 Sound_ = Sound()
 Display_ = Display()
 Sound_.play_tone(frequency=400, duration=0.5, volume=50) #plays a note so we know when the code starts
-motorA = MediumMotor(OUTPUT_A)
-motorD = MediumMotor(OUTPUT_D)
 #---------------------------------------------------------------------------------------------------------------------------------------------------
 
 #------------------------------------------------Distance conversion--------------------------------------------------------------------------------
@@ -38,78 +41,104 @@ def cmToRotations(cm):
 def inToMillimeters(inches):
     return inches * 25.4
 # centimeters to millimeters:
-def cmToMillimeters(cm):
+def cmToMillimeters(cm): #hhm it works... questionable -- no syntax errors!
     return cm * 10 # Yay, no syntax errors!
-#--------------------------------------------------------------------------------------------------------------------------------------------------
+def drive_cm(power, cm):
+    rt = cmToRotations(cm)
+    tank_drive.on_for_rotations(SpeedPercent(power), SpeedPercent(power), int(rt) )
+#---------------------------------------------------------------------------------------------------------------------------------------------
 
-#-----------------------------------yellow = swing and safety by Alan and Kunal--------------------------------------------------------------------
+#-----------------------------------yellow = swing and safety by Alan and Kunal---------------------------------------------------------------
 def swing_and_safety():
     tank_drive.on_for_rotations(SpeedPercent(50), SpeedPercent(50), 6.67) #ROBOT MOVES FORWARD FROM BASE
     tank_drive.on_for_rotations(SpeedPercent(20), SpeedPercent(20), .8) # ROBOT MOVES INTO SWING
     tank_drive.on_for_rotations(SpeedPercent(-30), SpeedPercent(-30), 0.4) #ROBOT MOVES AWAY FROM SWING
     tank_drive.on_for_rotations(SpeedPercent(-30), SpeedPercent(0), 1.5) #ROBOT TURNS TO SQUARE ON WALL
     motorA.on_for_degrees(SpeedPercent(15), 150) #LEFT ARM TURNS FOR ELEVATOR
-    tank_drive.on_for_rotations(SpeedPercent(-15), SpeedPercent(-15), 0.3) # ROBOT MOVES BACK INTO WALL
-    tank_drive.on_for_rotations(SpeedPercent(30), SpeedPercent(30), 1.8) 
-    tank_drive.on_for_rotations(SpeedPercent(30), SpeedPercent(0), 1) #
-    tank_drive.on_for_rotations(SpeedPercent(30), SpeedPercent(30), 1.25) 
-    motorA.on_for_degrees(SpeedPercent(15), 50)
-    motorA.on_for_degrees(SpeedPercent(15), 150)
-    tank_drive.on_for_rotations(SpeedPercent(0), SpeedPercent(-30), 0.8)
-    tank_drive.on_for_rotations(SpeedPercent(15), SpeedPercent(15), 1.05)
-    tank_drive.on_for_rotations(SpeedPercent(-10), SpeedPercent(10), 0.25)
-    tank_drive.on_for_rotations(SpeedPercent(-30), SpeedPercent(0), 0.5) 
-    tank_drive.on_for_rotations(SpeedPercent(-30), SpeedPercent(-30), 10) 
-#--------------------------------------------------------------------------------------------------------------------------------------------------
+    tank_drive.on_for_rotations(SpeedPercent(-15), SpeedPercent(-15), 0.45) # ROBOT MOVES BACK INTO WALL
+    tank_drive.on_for_rotations(SpeedPercent(30), SpeedPercent(30), 1.8) #ROBOT MOVES FORWARD TO ELEVATOR
+    tank_drive.on_for_rotations(SpeedPercent(30), SpeedPercent(0), 1) #ROBOT TURNS CLOCKWISE TO FACE ELEVATOR
+    tank_drive.on_for_rotations(SpeedPercent(30), SpeedPercent(30), 1.25) #ROBOT MOVES FORWARD AND HITS ELEVATOR
+    motorA.on_for_degrees(SpeedPercent(15), 200)#MEDIUM MOTOR TURNS AWAY SO IT DOESN'T UNDO ELEVATOR
+    tank_drive.on_for_rotations(SpeedPercent(0), SpeedPercent(-30), 0.8)#ROBOT TURNS TO SAFETY FACTOR
+    tank_drive.on_for_rotations(SpeedPercent(15), SpeedPercent(15), 1.13)#ROBOT MOVES INTO SAFETY FACTOR
+    tank_drive.on_for_rotations(SpeedPercent(10), SpeedPercent(-10), 0.2)#ROBOT TURNS TO KNOCK DOWN BEAMS
+    tank_drive.on_for_rotations(SpeedPercent(-15), SpeedPercent(-15), 0.3) # ROBOT MOVES BACK TO NOT KNOCK DOWN THE BUILDING IN SAFETY FACTOR
+    tank_drive.on_for_rotations(SpeedPercent(-10), SpeedPercent(10), 0.5)#ROBOT TURNS TO KNOCK DOWN BEAMS
+    tank_drive.on_for_rotations(SpeedPercent(-60), SpeedPercent(-60), 12) # ROBOT MOVES BACK TO BASE
+#------------------------------------------------------------------------------------------------------------------------------------------
 
-#green = backwards
-def green():
-    tank_drive.on_for_rotations(SpeedPercent(-50), SpeedPercent(-50), 1)
+#------------------------------------------------- Big Design and Build is green ----------------------------------------------------------
 
-#red = turn *
-def red():
-    tank_drive.on_for_rotations(SpeedPercent(0), SpeedPercent(50), 1)
+def big_design_and_build():
+    drive_cm(50, 65)
+    tank_drive.on_for_seconds(SpeedPercent(-10), SpeedPercent(-50), 2)
+    drive_cm(-20, 65)
 
-#----------------------------------------blue = crane by Ben and Joshua *----------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------ red = Design & Build 1 --------------------------------------------------------
+
+def design_and_build_one():
+    #go forward
+    tank_drive.on_for_degrees(SpeedPercent(50), SpeedPercent(50), 360)
+    #turn right
+    tank_drive.on_for_degrees(SpeedPercent(50), SpeedPercent(0), 545)
+    #move forward
+    tank_drive.on_for_degrees(SpeedPercent(50), SpeedPercent(50), 1322)
+    #turn left
+    tank_drive.on_for_degrees(SpeedPercent(0), SpeedPercent(50), 253)
+    #go forward
+    tank_drive.on_for_degrees(SpeedPercent(50), SpeedPercent(50), 669)
+    #go backward after leaving tan load, dropping off red load
+    tank_drive.on_for_degrees(SpeedPercent(50), SpeedPercent(50), -726)
+    #turn 
+    tank_drive.on_for_degrees(SpeedPercent(50), SpeedPercent(0), 839)
+    tank_drive.on_for_degrees(SpeedPercent(50), SpeedPercent(50), 600)
+    tank_drive.on_for_degrees(SpeedPercent(50), SpeedPercent(0), 510)
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+#---------------------------------------- blue = crane by Ben and Joshua --------------------------------------------------------------------
 def crane():
-    # 22 1/4 in forward
-    tank_drive.on_for_rotations(SpeedPercent(50), SpeedPercent(50), inToRotations(22.25))
-    # Move attachment down
-    motorD.on_for_seconds(SpeedPercent(-8), 1.8)
-    # Drive forward a bit
-    tank_drive.on_for_seconds(SpeedPercent(20), SpeedPercent(15), 0.3)
-    tank_drive.on_for_rotations(SpeedPercent(10), SpeedPercent(10), inToRotations(2.5)) 
-    tank_drive.on_for_seconds(SpeedPercent(21), SpeedPercent(17), 0.1)
-    tank_drive.on_for_rotations(SpeedPercent(10), SpeedPercent(10), inToRotations(2.25))
-    # Drive back a bit
-    tank_drive.on_for_rotations(SpeedPercent(10), SpeedPercent(10), inToRotations(-3))
-    motorD.on_for_seconds(SpeedPercent(8), 1.6)
-    # Push it down fully
-    tank_drive.on_for_rotations(SpeedPercent(12), SpeedPercent(12), inToRotations(1.5))
-    motorD.on_for_seconds(SpeedPercent(-15), 1.5)
-#-----------------------------------------------------------------------------------------------------------------------------------------
+    tank_drive.on_for_rotations(SpeedPercent(35), SpeedPercent(35), inToRotations(27))
+    tank_drive.on_for_rotations(SpeedPercent(20), SpeedPercent(20), inToRotations(-5))
+    tank_drive.on_for_seconds(SpeedPercent(10), SpeedPercent(20), 2.5)
+    tank_drive.on_for_rotations(SpeedPercent(20), SpeedPercent(20), inToRotations(3))
+    tank_drive.on_for_seconds(SpeedPercent(10), SpeedPercent(7), 1.5)
+    drive_cm(-15, 50)
+#--------------------------------------------------------------------------------------------------------------------------------------------
 
-#-----------------------------------------definning what "ColorChecking" is---------------------------------------------------------------
+#---------------------------------------- creating the function ColorChecking ---------------------------------------------------------------
 def ColorChecking():
     if color.color == color.COLOR_YELLOW: #if yellow
         swing_and_safety()
     elif color.color == color.COLOR_GREEN: #if green
-        green()
+        big_design_and_build()
     elif color.color == color.COLOR_RED: #if red
-        red()
+        design_and_build_one()
     elif color.color == color.COLOR_BLUE: #if blue
         crane()
-#-----------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------------------
 
 #This is where the movement happens. the function "ColorChecking" is a function to decide what to do based on color.
 
-#now whenever we touch enter (or the mibble button) then it will call ColorChecking().
+#--------------- failsafe -------------
+def failsafe():
+    sys.exit()
+#--------------------------------------
+
+#now whenever we touch enter (or the middle button) then it will call ColorChecking().
 Sound_.play_tone(frequency=400, duration=0.5, volume=50)
 start = time.time()
+btn.on_backspace = failsafe
 
-while (time.time() - start) <= 160: #this code essentially waits the duration of the match
+while True: #this code essentially color checks forever
+    if (btn.down):
+        break
     pass
-    btn.wait_for_released('right')
+    btn.wait_for_released('enter')
     ColorChecking()
 
+# Beepity beep!
 Sound_.play_tone(frequency=400, duration=0.5, volume=50)
